98a2a8f5fb0d52015cbc8244e55998c4d2ac6418
Jose Miguel <josemptj@gmail.com>
2023-07-10 10:55:34 -0300

    first commit

---
+++ movie-client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="title-app">Aca se desarrollara una app de peliculas 🎞️</h1>
+    </>
+  );
+}
+
+export default App;
